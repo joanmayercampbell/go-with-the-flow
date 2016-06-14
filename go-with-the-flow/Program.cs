32a7e2f48b1c7667e63a0a7eb23251f65330d584
Joan Mayer Campbell <joanmayercampbell@gmail.com>
2016-06-14 16:04:01 -0400

    initial attempt

---
+++ go-with-the-flow/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace go_with_the_flow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How to decide what to eat");
+            Console.WriteLine("Did this food grow from the earth or have a mother?");
+            string answer = Console.ReadLine();
+            if (answer.ToLower() == "yes")
+            {
+                Console.WriteLine("Does this food come from a package or have a Label?");
+                answer = Console.ReadLine();
+            //    if (answer.ToLower() == "yes")
+              //  {
+                //    Console.WriteLine("Does it have more that 5 indgredients?");
+                  //  answer = Console.ReadLine();
+                    //if (answer.ToLower() == "yes")
+           //         {
+           //             Console.WriteLine("Not recommended to eat!");
+           //
+           //       }
+           //         else
+           //        {
+           //             Console.WriteLine("Bon Appetite!!");
+           //         }
+           //    else {
+           //             Console.WriteLine("Is it raised naturally?");
+           //             answer = Console.ReadLine();
+           //            if (answer.ToLower() == "yes")
+           //             {
+           //                 Console.WriteLine("Bon Appetite!");
+           //             }
+           //             else
+           //             {
+           //                 Console.WriteLine("Don't eat!!");
+           //             }
+           //             else{
+           //                 Console.WriteLine("Don't eat!!");
+           //             }
+           //
+           //         }
+           //         else {
+
+           //         }
+
+
+           //     }
+           // }
+           // else
+           // {
+           //     Console.WriteLine("Do not eat!!");
+            }
+
+        }
+    }
+}
